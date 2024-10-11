0fa3007ec684c087628d5145dd3a7b2e9be073f3
Gilang <gilangnurrizki173@gmail.com>
2024-10-11 18:50:00 +0700

    update favorite view model

--- app/src/main/java/com/example/dicodingeventapp/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/dicodingeventapp/ui/favorite/FavoriteViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.dicodingeventapp.ui.favorite
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.dicodingeventapp.data.EventRepository
+import com.example.dicodingeventapp.data.local.FavoriteRepository
 import com.example.dicodingeventapp.data.local.entity.FavoriteEvent
 
-class FavoriteViewModel(private val eventRepository: EventRepository) : ViewModel() {
-    val favoriteEvents: LiveData<List<FavoriteEvent>> = eventRepository.getAllFavorite()
+class FavoriteViewModel(application: Application) : ViewModel() {
+    private val favoriteRepository: FavoriteRepository = FavoriteRepository(application)
+
+    fun getAllFav(): LiveData<List<FavoriteEvent>> {
+        return favoriteRepository.getAllFavEvent()
+    }
 
 }
