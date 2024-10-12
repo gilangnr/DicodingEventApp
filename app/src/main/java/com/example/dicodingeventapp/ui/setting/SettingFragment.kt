cc2da6921d74663fccf4e01a755b3afe3b70ec77
Gilang <gilangnurrizki173@gmail.com>
2024-10-12 22:23:49 +0700

    fix bug bottom navigation on darkmode

--- app/src/main/java/com/example/dicodingeventapp/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/dicodingeventapp/ui/setting/SettingFragment.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.dicodingeventapp.R
+import com.example.dicodingeventapp.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
+
+    private var _binding : FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switchTheme.isChecked = false
+            }
+        }
+    }
 
 }
