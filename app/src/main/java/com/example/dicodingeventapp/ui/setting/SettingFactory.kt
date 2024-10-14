0af8884d2f1133363510c3d8139008ded97a6edb
Gilang <gilangnurrizki173@gmail.com>
2024-10-14 10:06:56 +0700

    push setting factory

---
+++ app/src/main/java/com/example/dicodingeventapp/ui/setting/SettingFactory.kt
@@ -0,0 +1,14 @@
+package com.example.dicodingeventapp.ui.setting
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class SettingFactory(private val pref: SettingPreferences): ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SettingViewModel::class.java)) {
+            return SettingViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
